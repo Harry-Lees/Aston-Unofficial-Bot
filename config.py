bcd63ad3ddf35332be32e3c8d14805a56ebe4deb
Harry <harry.lees@gmail.com>
2020-11-02 03:25:35 +0000

    Update config.py

--- config.py
+++ config.py
@@ -30,7 +30,7 @@ class Config:
 
 
 class HerokuConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgres://vhezruoasdndgj:13b653848bb7551deb646ebc3767413bb1331adc5de6817d59eeaf20654479dc@ec2-54-75-229-28.eu-west-1.compute.amazonaws.com:5432/d4f3k72v7ujiu5'
+    SQLALCHEMY_DATABASE_URI = ''
     
 
 class ProductionConfig(Config):
