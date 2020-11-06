fdac1463c7fa7c49a56c459634d504a13848d445
Harry Lees <harry.lees@gmail.com>
2020-11-06 15:59:18 +0000

    added logging

--- wsgi.py
+++ wsgi.py
@@ -6,6 +6,7 @@ app = create_app()
 __author__ = 'Harry Lees'
 __date__ = '20.10.2020'
 
+print(getenv('PORT'))
+
 if __name__ == '__main__':
-    print(getenv('PORT'))
     app.run(host='0.0.0.0', port = getenv('PORT', 5000))
