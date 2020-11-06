93c6c67fc81f41206cf629d4c3b3e22be5935cec
Harry Lees <harry.lees@gmail.com>
2020-11-06 00:34:47 +0000

    added PORT to environment variables

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,5 @@
 from app import create_app
+from os import getenv
 
 app = create_app()
 
@@ -6,4 +7,4 @@ __author__ = 'Harry Lees'
 __date__ = '20.10.2020'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port = getenv('PORT', 5000))
