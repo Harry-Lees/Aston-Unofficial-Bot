48486e8b0458ed51bde172dca89f5874b2e60412
Harry Lees <harry.lees@gmail.com>
2020-11-13 00:23:08 +0000

    updated app

--- app/user/routes.py
+++ app/user/routes.py
@@ -17,7 +17,8 @@ def callback():
 
 @blueprint.route('/login')
 def login():
-    return discord.create_session(scope=['identify', 'guilds'])
+    print(app.config)
+    return discord.create_session(scope = ['identify', 'guilds'])
 
 
 @blueprint.route('/logout')
@@ -44,50 +45,3 @@ def me():
             <img src='{user.avatar_url}' />
         </body>
     </html>"""
-
-
-# @blueprint.route('/register', methods = ['GET', 'POST'])
-# def register():
-#     print('called')
-#     print(request.method)
-#     form = RegisterForm()
-
-#     if current_user.is_authenticated:
-#         print('already authenticated')
-#         return redirect(url_for('core.index'))
-
-#     if form.validate_on_submit():
-#         print('valid form')
-#         hashed_password = bcrypt.generate_password_hash(form.password.data)
-#         user = User(username = form.username.data, email = form.email.data, password = hashed_password)
-
-#         database.session.add(user)
-#         database.session.commit()
-
-#         flash('Sucessfully created account!')
-#         return redirect(url_for('login'))
-
-#     return render_template('register.html', form = form)
-
-# @blueprint.route('/login', methods = ['GET', 'POST'])
-# def login():
-#     form = LoginForm()
-
-#     if current_user.is_authenticated:
-#         return redirect(url_for('core.setup'))
-
-#     if form.validate_on_submit():
-#         user = User.query.filter_by(username = form.username.data).first()
-
-#         if not user:
-#             flash('There is no user with that username, please try again.')
-#             return render_template('login.html', form = form)
-
-#         correct_password = bcrypt.check_password_hash(user.password, form.password.data)
-#         if correct_password:
-#             login_user(user, remember = form.remember.data)
-#             return redirect(url_for('core.setup'))
-#         else:
-#             flash('Login unsuccessful, please check password.')
-
-#     return render_template('login.html', form = form)
