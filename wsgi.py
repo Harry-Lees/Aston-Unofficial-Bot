bc3b7912e600f3a187e1e44b8ae388ab517c8678
Harry Lees <harry.lees@gmail.com>
2020-11-13 00:35:20 +0000

    added proxy to fix discord OAuth2

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,9 @@
 from app import create_app
 from os import getenv
+from werkzeug.middleware.proxy_fix import ProxyFix 
 
 app = create_app()
+app = ProxyFix(app, x_for=1, x_host=1)
 
 __author__ = 'Harry Lees'
 __date__ = '20.10.2020'
