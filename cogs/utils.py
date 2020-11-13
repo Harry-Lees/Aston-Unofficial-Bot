057eb83b648c9af9304b8ae1a5c42bb7f13c9535
Harry Lees <harry.lees@gmail.com>
2020-11-13 21:25:15 +0000

    added \!join_log command

--- cogs/utils.py
+++ cogs/utils.py
@@ -1,11 +1,18 @@
 from os import remove
 
 import discord
+import matplotlib
+
+matplotlib.use('Agg')
+from collections import Counter
+from datetime import datetime, timedelta
+from os import remove
+
 import matplotlib.pyplot as plt
+from config import DiscordConfig
 from discord.ext import commands
 from discord.utils import get
 
-from config import DiscordConfig
 
 def setup(bot: object) -> None:
     bot.add_cog(Utils(bot))
@@ -62,3 +69,36 @@ class Utils(commands.Cog):
         embed.set_thumbnail(url = author.guild.icon_url)
         
         await ctx.send(embed = embed)
+
+
+    @commands.command('join_log')
+    @commands.has_role(DiscordConfig.ADMIN_ROLE)
+    async def join_log(self, ctx: object):
+        filename = 'temp.png'
+        author = ctx.message.author
+        guild = author.guild
+        
+        async with ctx.typing():
+            joined_dates = Counter([member.joined_at for member in guild.members])
+
+            start_date = min(joined_dates)
+            end_date = datetime.today()
+            delta = end_date - start_date
+
+            dates = [start_date + timedelta(days = i) for i in range(delta.days + 1)]
+            values = [joined_dates.get(date, 0) for date in dates]
+
+            fig, ax = plt.subplots()
+
+            ax.plot(dates, values)
+            ax.set_ylabel('People Joined')
+            ax.set_xlabel('Date')
+            ax.set_title('Number of People joined over time')
+            ax.grid(True)
+
+            fig.autofmt_xdate()
+            fig.tight_layout()
+            fig.savefig(filename)
+
+            await ctx.send(file = discord.File(filename))
+            remove(filename)
