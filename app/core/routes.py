0602ad81a486f8dd7e579fb2db19b12208df016e
Harry Lees <harry.lees@gmail.com>
2020-11-16 02:28:37 +0000

    website redesign + refactor

--- app/core/routes.py
+++ app/core/routes.py
@@ -7,7 +7,7 @@ blueprint = Blueprint('core', __name__, template_folder = 'templates')
 
 
 @blueprint.route('/')
-def return_index():
+def index():
     announcements = Announcements.query.all()
     return render_template('index.html', announcements = announcements)
 
@@ -15,6 +15,27 @@ def return_index():
 @blueprint.route('/feedback')
 def feedback():
     return render_template('feedback.html')
+
+
+@blueprint.route('/commands')
+def commands():
+    commands = [
+        ['verify', 'Manually verify a user', 'verify @user'],
+        ['get_link', 'Gets a users verification link', 'get_link @user'],
+        ['self_link', 'Gets your own verification link', 'self_link'],
+    ]
+
+    return render_template('commands.html', commands = commands)
+
+
+@blueprint.route('/status')
+def status():
+    return render_template('status.html')
+
+
+@blueprint.route('/help')
+def help_page():
+    return render_template('help.html')
 
 
 @blueprint.errorhandler(404)
